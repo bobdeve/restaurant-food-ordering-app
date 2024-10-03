1dde83ef92a9201aac69227b8d8fa91b7553c4c3
bobdeve <122013187+bobdeve@users.noreply.github.com>
2024-10-03 15:43:42 +0300

    changed it from local host to render server

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -78,7 +78,7 @@ export const CheckOut = () => {
       "Content-Type": "application/json"
     }
    // http://localhost:3000/create-checkout-session
-    const response = await fetch ("http://localhost:3000/create-checkout-session",{
+    const response = await fetch ("https://demo-foodorder-3.onrender.com/create-checkout-session",{
       method: 'POST',
       headers: headers,
       body:JSON.stringify(body)
