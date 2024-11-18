ae648d73d8079cd289c0203f0e90babc2f44f16d
bobdeve <122013187+bobdeve@users.noreply.github.com>
2024-11-18 12:10:23 +0300

    updated header
    make the header sticky

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,16 +28,16 @@ export const Header = () => {
   }
   
   return (
-    <div className='flex flex-col md:flex-row justify-between mx-4 md:mx-24 my-4 items-center'>
-        <div className='flex gap-4 items-center mb-4 md:mb-0'>
-            <img className='w-24 rounded-[50%] border-[3px] border-[#FFC404]' src={logoPic} alt="Logo of the food application" />
-            <h1 className='text-center md:text-left text-lg md:text-xl'>Tasty Delivery</h1>
-        </div>
-        
-        {/* <Button onClick={showUserCart} label="main">Cart({totalNumberOfItems})</Button> */}
-        <Button onClick={showUserCart} label="main">
-    <FontAwesomeIcon icon={faShoppingCart} /> {totalNumberOfItems}
-</Button>
+    <div className='flex flex-col md:flex-row justify-between mx-4 md:mx-24 my-4 items-center sticky top-0 bg-[#020202] z-50 p-4 rounded-md'>
+    <div className='flex gap-4 items-center mb-4 md:mb-0'>
+      <img className='w-24 rounded-[50%] border-[3px] border-[#FFC404]' src={logoPic} alt="Logo of the food application" />
+      <h1 className='text-center md:text-left text-lg md:text-xl'>Tasty Delivery</h1>
     </div>
+  
+    <Button onClick={showUserCart} label="main">
+      <FontAwesomeIcon icon={faShoppingCart} /> {totalNumberOfItems}
+    </Button>
+  </div>
+  
   )
 }
